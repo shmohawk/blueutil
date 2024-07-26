4aa55b653a59c9121ac7fa57120a26960f89505e
shmohawk <hawkshmo@gmail.com>
2024-07-26 16:21:33 +0200

    Create blueutil.h

---
+++ blueutil.h
@@ -0,0 +1,16 @@
+//
+//  blueutil.h
+//  blueutil
+//
+//  Created by shmohawk on 7/26/24.
+//
+//
+
+#ifndef blueutil_blueutil_h
+#define blueutil_blueutil_h
+
+int BTPowerState();
+int BTSetPowerState(int powerState);
+void BTStatus();
+
+#endif
